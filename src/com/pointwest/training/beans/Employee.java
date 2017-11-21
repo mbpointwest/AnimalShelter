4906353421088e14ecc00b481010bc648ed98d91
miguel.barba <miguel.barba@pointwest.com.ph>
2017-11-21 19:42:04 +0800

    added employee branch

---
+++ src/com/pointwest/training/beans/Employee.java
@@ -0,0 +1,18 @@
+public class Employee {
+
+private int employeeId;
+
+public Employee() {
+this.employeeId = 1;
+}
+
+
+public void setEmployeeID (int employeeId){
+this.employeeId = employeeId;
+}
+
+public int getEmployeeId(){
+ return employeeId;
+}
+
+}
